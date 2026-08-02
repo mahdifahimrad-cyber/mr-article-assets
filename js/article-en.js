d6e13e4d953477e879e47737bf893f09868545d1
mahdifahimrad-cyber <mahdi.fahimrad@gmail.com>
2026-08-02 03:15:11 -0700

    Update version comment in article-en.js to v3

--- js/article-en.js
+++ js/article-en.js
@@ -1,4 +1,4 @@
-/*Version 2026-08-01-v2*/
+/*Version 2026-08-01-v3*/
 (function () {
         var container = document.currentScript && document.currentScript.parentElement;
         if (!container) return;
